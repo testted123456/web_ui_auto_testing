82c96b694d65f6d6761e6e9164becb14dbe6a9c4
user <hanjunchao@nonobank.com>
2016-09-29 19:41:02 +0800

    增加相关case

--- src/main/java/com/nonobank/apps/page/account/Page_Account.java
+++ src/main/java/com/nonobank/apps/page/account/Page_Account.java
@@ -19,32 +19,26 @@ public class Page_Account extends BasePage {
 		WebLink link_bank_account = objectFactory.getWebLink("Banks");
 		link_bank_account.click();
 	}
-
 	// 点击提现
 	public void click_withdrawal() {
 		WebButton link_withdrawal = objectFactory.getWebButton("withdrawal");
 		link_withdrawal.click();
 	}
-
 	// 点击充值
 	public void click_recharge() {
 		WebButton link_recharge = objectFactory.getWebButton("recharge");
 		link_recharge.click();
 	}
-
-	// *****************************身份认证***************************************//
 	// 点击认证管理
 	public void click_degreeCard() {
 		WebLink link_degreeCard = objectFactory.getWebLink("degreeCard");
 		link_degreeCard.click();
 	}
-
 	// 输入真实姓名
 	public void input_name(String myname) {
 		WebInput input_myname = objectFactory.getWebInput("myname");
 		input_myname.clearAndInput(myname);
 	}
-
 	// 输入身份证号
 	public void input_mycard(String mycard) {
 		WebInput input_mycard = objectFactory.getWebInput("mycard");
@@ -70,53 +64,52 @@ public class Page_Account extends BasePage {
 		}
 		return false;
 	}
-
-	// *****************************设置支付密码***************************************//
-
 	// 点击个人设置
 	public void click_profile() {
 		WebLink link_profile = objectFactory.getWebLink("profile");
 		link_profile.click();
 	}
-
 	// 点击密码设置
 	public void click_passwordSetting() {
 		WebCommon li_passwordSetting = objectFactory.getWebCommon("passwordSetting");
 		li_passwordSetting.click();
 	}
-
 	// 点击密码设置
 	public void click_paypasswordSetting() {
-		// List<WebElement> list =
-		// objectFactory.getWebElements(By.xpath("//li[@class='active']"));
-		// for(WebElement element : list){
-		// String text = element.getText();
-		// if(text.equals("修改支付密码")){
-		// element.click();
-		// break;
-		// }
-		// }
 		WebCommon li_payPasswordSetting = objectFactory.getWebCommon("payPasswordSetting");
 		li_payPasswordSetting.click();
 	}
-
 	// 输入支付密码
 	public void input_payPassword(String payPassword) {
 		WebInput input_payPayassword = objectFactory.getWebInput("newZFPwd");
 		input_payPayassword.clearAndInput(payPassword);
 	}
-
 	// 输入支付密码
 	public void input_payPassword_again(String payPassword) {
 		WebInput input_payPassword_again = objectFactory.getWebInput("newZFPassword");
 		input_payPassword_again.clearAndInput(payPassword);
 	}
-
 	// 确认修改密码
 	public void setPasswordbtn() {
 		WebInput setPasswordbtn = objectFactory.getWebInput("setPasswordbtn");
 		setPasswordbtn.click();
 	}
-
-
+	//点击借款记录
+	public void click_borrowsRecord(){
+		logger.info("点击借款记录.....");
+		WebCommon click_borrowsRecord=objectFactory.getWebCommon("借款记录");
+		click_borrowsRecord.click();
+	}
+	//点击我的账单
+	public void click_myBill(){
+		logger.info("点击我的账单.....");
+		WebCommon click_myBill=objectFactory.getWebCommon("我的账单");
+		click_myBill.click();
+	}
+	//点击借款资料
+	public void click_borrowsData(){
+		logger.info("点击我的账单.....");
+		WebCommon click_borrowsData=objectFactory.getWebCommon("借款资料");
+		click_borrowsData.click();
+	}
 }
