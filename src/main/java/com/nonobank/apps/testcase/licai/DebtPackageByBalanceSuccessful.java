3ecceead46ec38f492611317475cd5c0ba0f5fee
lishuangshuang <lishuangshuang@nonobank.com>
2016-10-25 20:59:34 +0800

    update

--- src/main/java/com/nonobank/apps/testcase/licai/DebtPackageByBalanceSuccessful.java
+++ src/main/java/com/nonobank/apps/testcase/licai/DebtPackageByBalanceSuccessful.java
@@ -1,31 +1,44 @@
 package com.nonobank.apps.testcase.licai;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import com.nonobank.apps.business.licai.Biz_Licai_FinancePlan;
 import com.nonobank.apps.business.licai.Biz_Licai_Order;
 import com.nonobank.apps.business.licai.Biz_Licai_Payment;
 import com.nonobank.apps.business.licai.Biz_Licai_Payment_Successful;
 import com.nonobank.apps.business.portal.Biz_Login;
+import com.nonobank.apps.business.portal.Biz_Portal;
+import com.nonobank.apps.business.recharge.Biz_User_Recharge;
+import com.nonobank.apps.business.recharge.Biz_User_RechargeConfirm;
 import com.nonobank.apps.testcase.base.BaseCase;
 
-public class DebtByOldBankCardSuccessful extends BaseCase {
+/**
+ * 用账户余额购买债券成功
+ * 
+ * @author ted
+ *
+ */
+public class DebtPackageByBalanceSuccessful extends BaseCase {
 
-	public static Logger logger = LogManager.getLogger(DebtByOldBankCardSuccessful.class);
 	Biz_Login biz_Login = new Biz_Login();
+	Biz_Portal biz_Portal = new Biz_Portal();
+	Biz_User_Recharge biz_User_Recharge = new Biz_User_Recharge();
+	Biz_User_RechargeConfirm biz_User_RechargeConfirm = new Biz_User_RechargeConfirm();
 	Biz_Licai_FinancePlan biz_Licai_FinancePlan = new Biz_Licai_FinancePlan();
 	Biz_Licai_Order biz_Licai_Order = new Biz_Licai_Order();
 	Biz_Licai_Payment biz_Licai_Payment = new Biz_Licai_Payment();
 	Biz_Licai_Payment_Successful biz_Licai_Payment_Successful = new Biz_Licai_Payment_Successful();
 
 	@Test(dataProvider = "dataSource")
-	public void test(String mobile, String password, String id, String amount, String cardNO, String payPassword,
-			String smsCode) {
+	public void test(String mobile, String password, String cardno, String money, String pay_password, String smsCode,
+			String id, String amount, String payPassword) {
 
 		biz_Login.login(mobile, password, "mobile_num");
-		biz_Licai_FinancePlan.purchase(id, amount, "/Debt/View/");
+		biz_Portal.navigate_to_myaccount();
+		biz_User_Recharge.recharge(cardno, mobile);
+		biz_User_RechargeConfirm.rechargeConfirm(money, pay_password, smsCode);
+		biz_Licai_FinancePlan.purchase(id, amount, "/Debt/ViewDebtPackage/");
 		biz_Licai_Order.submit();
 		biz_Licai_Payment.payByBalance(payPassword);
+
 	}
 }
--- src/main/java/com/nonobank/apps/testcase/licai/DebtPackageByOldBankCardSuccessful.java
+++ src/main/java/com/nonobank/apps/testcase/licai/DebtPackageByOldBankCardSuccessful.java
@@ -10,9 +10,9 @@ import com.nonobank.apps.business.licai.Biz_Licai_Payment_Successful;
 import com.nonobank.apps.business.portal.Biz_Login;
 import com.nonobank.apps.testcase.base.BaseCase;
 
-public class DebtByNewBankCardSuccessful extends BaseCase {
+public class DebtPackageByOldBankCardSuccessful extends BaseCase {
 
-	public static Logger logger = LogManager.getLogger(DebtByNewBankCardSuccessful.class);
+	public static Logger logger = LogManager.getLogger(DebtPackageByOldBankCardSuccessful.class);
 	Biz_Login biz_Login = new Biz_Login();
 	Biz_Licai_FinancePlan biz_Licai_FinancePlan = new Biz_Licai_FinancePlan();
 	Biz_Licai_Order biz_Licai_Order = new Biz_Licai_Order();
@@ -20,11 +20,11 @@ public class DebtByNewBankCardSuccessful extends BaseCase {
 	Biz_Licai_Payment_Successful biz_Licai_Payment_Successful = new Biz_Licai_Payment_Successful();
 
 	@Test(dataProvider = "dataSource")
-	public void test(String mobile, String password, String id, String amount, String bank_name, String cardNO,
-			String validationCode, String payPassword) {
+	public void test(String mobile, String password, String id, String amount, String cardNO, String payPassword,
+			String smsCode) {
 
 		biz_Login.login(mobile, password, "mobile_num");
-		biz_Licai_FinancePlan.purchase(id, amount, "/Debt/View/");
+		biz_Licai_FinancePlan.purchase(id, amount, "/Debt/ViewDebtPackage/");
 		biz_Licai_Order.submit();
 		biz_Licai_Payment.payByBalance(payPassword);
 	}
