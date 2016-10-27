6c4d0afc809067ebe0a857ebf12047dd60c8f969
lishuangshuang <lishuangshuang@nonobank.com>
2016-10-27 20:12:35 +0800

    update

--- src/main/java/com/nonobank/apps/testcase/licai/LingyuanByNewBankCardSuccessful.java
+++ src/main/java/com/nonobank/apps/testcase/licai/LingyuanByNewBankCardSuccessful.java
@@ -3,30 +3,39 @@ package com.nonobank.apps.testcase.licai;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
+import com.nonobank.apps.business.account.Biz_Account;
 import com.nonobank.apps.business.licai.Biz_Licai_FinancePlan;
 import com.nonobank.apps.business.licai.Biz_Licai_Order;
 import com.nonobank.apps.business.licai.Biz_Licai_Payment;
 import com.nonobank.apps.business.licai.Biz_Licai_Payment_Successful;
-import com.nonobank.apps.business.portal.Biz_Login;
+import com.nonobank.apps.business.portal.Biz_Portal;
+import com.nonobank.apps.business.portal.Biz_Register;
 import com.nonobank.apps.testcase.base.BaseCase;
 
 public class LingyuanByNewBankCardSuccessful extends BaseCase {
 
 	public static Logger logger = LogManager.getLogger(LingyuanByNewBankCardSuccessful.class);
-	Biz_Login biz_Login = new Biz_Login();
+	Biz_Register biz_Register;
+	Biz_Portal biz_Portal;
+	Biz_Account biz_Account;
+
 	Biz_Licai_FinancePlan biz_Licai_FinancePlan = new Biz_Licai_FinancePlan();
 	Biz_Licai_Order biz_Licai_Order = new Biz_Licai_Order();
 	Biz_Licai_Payment biz_Licai_Payment = new Biz_Licai_Payment();
 	Biz_Licai_Payment_Successful biz_Licai_Payment_Successful = new Biz_Licai_Payment_Successful();
 
 	@Test(dataProvider = "dataSource")
-	public void test(String mobile, String password, String id, String amount, String bank_name, String cardNO,
-			String payPassword) {
-		biz_Login.login(mobile, password, "mobile_num");
+	public void test(String mobile, String user_name, String password, String password2, String myname,
+			String identity_ID, String payPassword, String payPassword2, String id, String amount, String bank_name,
+			String cardNo) {
+		biz_Register.register(mobile, user_name, password, password2);
+		biz_Portal.close_dialog();
+		biz_Portal.navigate_to_myaccount();
+		biz_Account.IDVerification(myname, identity_ID);
+		biz_Account.setPayPassword(payPassword, payPassword2);
 
 		biz_Licai_FinancePlan.purchase(id, amount, "/Licai/FinancePlan/");
 		biz_Licai_Order.submit();
-		biz_Licai_Payment.payByNewCard(bank_name, cardNO, payPassword);
-
+		biz_Licai_Payment.payByNewCard(bank_name, cardNo, payPassword);
 	}
 }
