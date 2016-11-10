6b9e5de39871722415200b95574b78c3fe1b6592
lishuangshuang <lishuangshuang@nonobank.com>
2016-11-10 16:45:55 +0800

    update

--- src/main/java/com/nonobank/apps/utils/data/Assertion.java
+++ src/main/java/com/nonobank/apps/utils/data/Assertion.java
@@ -12,16 +12,16 @@ public class Assertion {
 
 		try {
 			Assert.assertEquals(expected, actual);
-			logger.info("检查点：" + msg + "成功");
+			logger.info("检查点：" + msg + "-----------成功");
 		} catch (Error e) {
-			logger.error("检查点：" + msg + "失败，预期值:" + expected + ",实际值:" + actual);
+			logger.error("检查点：" + msg + "-----------失败，预期值:" + expected + ",实际值:" + actual);
 			Assert.fail();
 		}
 	}
 
 	public static void assertEquals(Class clazz, String msg) {
 		logger = LogManager.getLogger(clazz);
-		logger.error(msg + "执行返回为空值");
+		logger.error(msg + "-----------执行返回为空值");
 		Assert.fail();
 	}
 
