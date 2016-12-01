e38b162638f1c5ce040df554933809d0d0a4374e
lishuangshuang <lishuangshuang@nonobank.com>
2016-12-01 16:22:27 +0800

    add

---
+++ src/main/java/com/nonobank/apps/business/user/Biz_Profile.java
@@ -0,0 +1,27 @@
+package com.nonobank.apps.business.user;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.nonobank.apps.page.user.Page_Profile;
+import com.nonobank.apps.utils.webintegration.Info;
+
+public class Biz_Profile {
+	public static Logger logger = LogManager.getLogger(Biz_Profile.class);
+	public Page_Profile page_Profile = new Page_Profile();
+
+	@Info(name = "nagivate_to_login", desc = "跳转到登录页面", dependency = "", isDisabled = false)
+	public void nagivate_to_profile() {
+		logger.info("跳转到个人设置页面............");
+		page_Profile.nagivate_to_profile();
+	}
+
+	// 设置支付密码
+	public void setPayPassword(String payPassword, String payPassword1) {
+		logger.info("设置支付密码...");
+		page_Profile.click_paypasswordSetting();
+		page_Profile.input_payPassword(payPassword);
+		page_Profile.input_payPassword_again(payPassword1);
+		page_Profile.setPasswordbtn();
+		page_Profile.closeAlert();
+	}
+}
---
+++ src/main/java/com/nonobank/apps/page/account/Page_DegreeCard.java
@@ -0,0 +1,42 @@
+package com.nonobank.apps.page.account;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.nonobank.apps.objectRepository.WebInput;
+import com.nonobank.apps.page.base.BasePage;
+import com.nonobank.apps.utils.file.ParseProperties;
+import com.nonobank.apps.utils.page.PageUtils;
+
+public class Page_DegreeCard extends BasePage {
+	public static Logger logger = LogManager.getLogger(Page_DegreeCard.class);
+
+	/**
+	 * 跳转到认证管理页面
+	 */
+	public void nagivate_to_degreeCard() {
+		String url_login = ParseProperties.getInstance().getProperty("url") + "/Account/degreeCard";
+		driver.navigate().to(url_login);
+		logger.info(PageUtils.getUrl());
+	}
+
+	// 输入真实姓名
+	public void input_name(String myname) {
+		logger.info("输入真实姓名....");
+		WebInput input_myname = objectFactory.getWebInput("myname");
+		input_myname.clearAndInput(myname);
+	}
+
+	// 输入身份证号
+	public void input_mycard(String mycard) {
+		logger.info("输入身份证号....");
+		WebInput input_mycard = objectFactory.getWebInput("mycard");
+		input_mycard.clearAndInput(mycard);
+	}
+
+	// 认证通过
+	public void submit() {
+		logger.info("点击提交....");
+		WebInput submit = objectFactory.getWebInput("ok");
+		submit.click();
+	}
+}
