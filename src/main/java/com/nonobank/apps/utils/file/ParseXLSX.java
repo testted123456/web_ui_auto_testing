716e96a87f51182f598dfe187b45eefb066bad9d
lishuangshuang <lishuangshuang@nonobank.com>
2017-01-14 21:20:20 +0800

    update

--- src/main/java/com/nonobank/apps/utils/file/ParseXLSX.java
+++ src/main/java/com/nonobank/apps/utils/file/ParseXLSX.java
@@ -373,7 +373,7 @@ public class ParseXLSX {
 	}
 
 	public static void main(String[] args) {
-		Object[][] objects = getDataValue("resources/TestData/com/nonobank/apps/testcase/bindcard/LoginBindCard.xlsx",
+		Object[][] objects = getDataValue("resources/TestData/com/nonobank/apps/testcase/bindcard/RegisterBindCard.xlsx",
 				"test");
 		System.out.println("******************objects=" + objects.length);
 		for (Object[] objects2 : objects) {
