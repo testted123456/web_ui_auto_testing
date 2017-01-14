a0d07501dc3e959b262cac19e522ccd26de78b9d
lishuangshuang <lishuangshuang@nonobank.com>
2017-01-14 17:19:23 +0800

    update

--- src/main/java/com/nonobank/apps/business/withdrawal/Biz_User_Withdrawal.java
+++ src/main/java/com/nonobank/apps/business/withdrawal/Biz_User_Withdrawal.java
@@ -24,16 +24,19 @@ public class Biz_User_Withdrawal {
 		try {
 			logger.info("选择银行卡、输入提现金额...");
 			page_User_Withdrawal.input_money(money);
+			PageUtils.sleep(2000);
 			page_User_Withdrawal.goNext();
+			PageUtils.sleep(2000);
 			page_User_Withdrawal.closeAlert();
 			PageUtils.sleep(2000);
-		} catch (Exception e) {
+		} catch (Error e) {
 			switch (message) {
 			case "提现金额必须为数字":
 			case "提现金额不能小于1元！":
 			case "本次最多可提现":
 				String error_msg = page_User_Withdrawal.getElementText("error_msg");
 				Assertion.assertEquals(error_msg.contains(message), true, Biz_Login.class, "反例-校验金额");
+				
 				break;
 			}
 		}
--- src/main/java/com/nonobank/apps/utils/data/BankCardUtils.java
+++ src/main/java/com/nonobank/apps/utils/data/BankCardUtils.java
@@ -106,7 +106,7 @@ public class BankCardUtils {
 	}
 
 	public static String getUsedBankCard(String bankCode) {
-		String sql = "select ubi.bank_card_no from user_info ui, user_bankcard_info ubi where  ui.id=ubi.user_id WHERE bank_code ='" + bankCode + "'  limit 1";
+		String sql = "select ubi.bank_card_no from user_info ui, user_bankcard_info ubi where  ui.id=ubi.user_id and bank_code ='" + bankCode + "'  limit 1";
 		Connection con = DBUtils.getConnection("nono");
 		String cardno = DBUtils.getOneObject(con, sql).toString();
 		DBUtils.closeConnection();
