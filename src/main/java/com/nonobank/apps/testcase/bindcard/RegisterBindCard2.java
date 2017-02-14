51e760db7bcf7e26e4ecffd173ae3e9e87150359
lishuangshuang <lishuangshuang@nonobank.com>
2017-02-14 15:01:24 +0800

    update

---
+++ src/main/java/com/nonobank/apps/testcase/bindcard/RegisterBindCard2.java
@@ -0,0 +1,50 @@
+package com.nonobank.apps.testcase.bindcard;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+import com.nonobank.apps.business.account.Biz_Account;
+import com.nonobank.apps.business.account.Biz_DegreeCard;
+import com.nonobank.apps.business.bank.Biz_User_Banks;
+import com.nonobank.apps.business.bank.Biz_User_BindCard1;
+import com.nonobank.apps.business.bank.Biz_User_BindCard2;
+import com.nonobank.apps.business.portal.Biz_Portal;
+import com.nonobank.apps.business.portal.Biz_Register;
+import com.nonobank.apps.business.user.Biz_Profile;
+import com.nonobank.apps.testcase.base.BaseCase;
+
+public class RegisterBindCard2 extends BaseCase {
+	public static Logger logger = LogManager.getLogger(RegisterBindCard2.class);
+	Biz_Portal biz_Portal;
+	Biz_Register biz_Register;
+	Biz_Account biz_Account;
+	Biz_Profile biz_Profile;
+	Biz_DegreeCard biz_DegreeCard;
+	Biz_User_Banks biz_User_Banks;
+	Biz_User_BindCard1 biz_User_BindCard1;
+	Biz_User_BindCard2 biz_User_BindCard2;
+
+	@Test(dataProvider = "dataSource")
+	public void test(String testcaseName, String testcaseDescription, String mobile, String user_name, String password,
+			String password2, String checkCode, String validation, String myname, String identity_ID,
+			String payPassword, String payPassword2, String bank_name, String bankcard_no, String validation2,
+			String expectResult) {
+		caseName = testcaseName;
+		caseDescription = testcaseDescription;
+		inputParams = mobile;
+		actualResult = "成功";
+		errorMessage = "无错误信息";
+
+		biz_Portal.navigate_to_register();
+		biz_Register.register(mobile, user_name, password, password2, checkCode, validation, "恭喜您注册成功，诺诺镑客因你而精彩！");
+		biz_Portal.navigate_to_myaccount();
+		biz_Account.navigate_to_profile();
+		biz_Profile.setPayPassword(payPassword, payPassword2);
+		biz_Account.navigate_to_degreeCard();
+		biz_DegreeCard.IDVerification(myname, identity_ID);
+		biz_Account.navigate_to_banks();
+		biz_User_Banks.navigate_to_bindCard1(expectResult);
+		biz_User_BindCard1.select_bank(bank_name);
+		biz_User_BindCard2.bindCard(bankcard_no, validation2, expectResult);
+	}
+}
